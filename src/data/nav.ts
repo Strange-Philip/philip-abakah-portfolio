76b1e5c4d22da316230cc953473fbf85a3179dfc
Philip Abakah <68474949+Strange-Philip@users.noreply.github.com>
2026-04-03 23:46:16 +0000

    Remove List from nav, fix impossible list item layout

--- src/data/nav.ts
+++ src/data/nav.ts
@@ -1,6 +1,5 @@
 export const navLinks = [
   { label: 'Works', href: '/works' },
-  { label: 'List', href: '/impossible-list' },
   { label: 'About', href: '/about' },
   { label: 'Now', href: '/now' },
 ] as const;
